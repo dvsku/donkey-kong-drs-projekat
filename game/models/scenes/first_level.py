1ddd814fba78d655de0d79c7726adbf7b0aeaddb
Jovanovski <jovanovski.david@yahoo.com>
2019-12-11 17:19:58 +0100

    Created first level design and added princess.

--- game/models/scenes/first_level.py
+++ game/models/scenes/first_level.py
@@ -13,12 +13,30 @@ class FirstLevel(GameScene):
 
         self.grid_painter.paint_horizontal_line(0, 19, 14, 0, 0, PaintObject.PLATFORM)
         self.grid_painter.paint_horizontal_line(3, 16, 10, 0, 0, PaintObject.PLATFORM)
+        self.grid_painter.paint_horizontal_line(9, 10, 8, 0, 0, PaintObject.PLATFORM)
+        self.grid_painter.paint_horizontal_line(10, 15, 6, 0, 0, PaintObject.PLATFORM)
+        self.grid_painter.paint_horizontal_line(0, 15, 3, 0, 0, PaintObject.PLATFORM)
+        self.grid_painter.paint_horizontal_line(5, 8, 6, 0, 0, PaintObject.PLATFORM)
 
         self.grid_painter.paint_vertical_line(2, 10, 13, 0, 0, PaintObject.LADDER)
         self.grid_painter.paint_vertical_line(17, 10, 13, 0, 0, PaintObject.LADDER)
+        self.grid_painter.paint_vertical_line(7, 10, 13, 0, 0, PaintObject.LADDER)
+        self.grid_painter.paint_vertical_line(12, 10, 13, 0, 0, PaintObject.LADDER)
+
+        self.grid_painter.paint_vertical_line(8, 8, 9, 0, 0, PaintObject.LADDER)
+        self.grid_painter.paint_vertical_line(11, 8, 9, 0, 0, PaintObject.LADDER)
+
+        self.grid_painter.paint_vertical_line(10, 6, 7, 0, 0, PaintObject.LADDER)
+
+        self.grid_painter.paint_vertical_line(15, 3, 5, 0, 0, PaintObject.LADDER)
+        self.grid_painter.paint_vertical_line(8, 3, 5, 0, 0, PaintObject.LADDER)
 
         self.grid_painter.paint_one(0, 13, 0, 5, PaintObject.PLAYER_1)
         self.grid_painter.paint_one(19, 13, 13, 5, PaintObject.PLAYER_2)
+
+        self.grid_painter.paint_one(5, 5, 0, 0, PaintObject.PRINCESS)
+
+
 
         # self.draw_item_to_scene(self.barrel_pool[0])
         # self.draw_item_to_scene(self.barrel_pool[1])
