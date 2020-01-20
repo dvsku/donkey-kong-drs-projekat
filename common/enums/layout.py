4d7fbd14ad3b2e260b35f33f0764f9611e0f91ca
Snežana Mrdalj <mrdaljsnezana@gmail.com>
2020-01-20 10:08:54 +0100

    Added levels layout

--- common/enums/layout.py
+++ common/enums/layout.py
@@ -6,6 +6,6 @@ from common.constants import COMMON_ROOT
 class Layouts(Enum):
     FirstLevel = COMMON_ROOT + "/layouts/first_level_layout.txt"
     SecondLevel = COMMON_ROOT + "/layouts/second_level_layout.txt"
-    ThirdLevel = COMMON_ROOT + "/layouts/first_level_layout.txt"
-    FourthLevel = COMMON_ROOT + "/layouts/first_level_layout.txt"
-    FifthLevel = COMMON_ROOT + "/layouts/first_level_layout.txt"
+    ThirdLevel = COMMON_ROOT + "/layouts/third_level_layout.txt"
+    FourthLevel = COMMON_ROOT + "/layouts/fourth_level_layout.txt"
+    FifthLevel = COMMON_ROOT + "/layouts/fifth_level_layout.txt"
